3873a2990da90df4923bd326d33fd4b048890d76
vvilll <156856594+vvilll@users.noreply.github.com>
2026-05-07 19:51:18 +0000

    changed db name to match .sql

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "projectnew";
+        $dbname = "cs3600db";
 
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
